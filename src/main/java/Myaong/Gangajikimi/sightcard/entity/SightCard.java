16777a606eb8705284cd996c6dc23ee9dd19a990
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-10-08 02:21:59 +0900

    [feature] 발견카드 엔티티 필드 추가

--- src/main/java/Myaong/Gangajikimi/sightcard/entity/SightCard.java
+++ src/main/java/Myaong/Gangajikimi/sightcard/entity/SightCard.java
@@ -3,6 +3,7 @@ package Myaong.Gangajikimi.sightcard.entity;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import Myaong.Gangajikimi.chatroom.entity.ChatRoom;
 import Myaong.Gangajikimi.common.BaseEntity;
 import Myaong.Gangajikimi.member.entity.Member;
 import Myaong.Gangajikimi.postlost.entity.PostLost;
@@ -54,6 +55,10 @@ public class SightCard extends BaseEntity {
 
 	@Column(length = 300, nullable = false)
 	private String foundPlace; // 카카오 역지오코딩 결과
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "chat_room_id", unique = true)
+	private ChatRoom chatRoom;  // 이 채팅방의 유일한 발견카드
 
 	// 게시글 작성자 id 반환 메서드
 	public Long getPostMemberId() {
