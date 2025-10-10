27ef34108eb36599f8a6d1f6f7d4d552bae7b2fa
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-10-10 21:24:41 +0900

    [feature] 외래키 제약 조건을 위해 OneToMany 어노테이션 및 필드 추가

--- src/main/java/Myaong/Gangajikimi/member/entity/Member.java
+++ src/main/java/Myaong/Gangajikimi/member/entity/Member.java
@@ -1,13 +1,24 @@
 package Myaong.Gangajikimi.member.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import Myaong.Gangajikimi.auth.web.dto.SignupRequest;
+import Myaong.Gangajikimi.chatroom.entity.ChatRoom;
 import Myaong.Gangajikimi.common.BaseEntity;
 import Myaong.Gangajikimi.common.enums.AccountStatus;
 import Myaong.Gangajikimi.common.enums.Role;
+import Myaong.Gangajikimi.postfound.entity.PostFound;
+import Myaong.Gangajikimi.postfoundreport.entity.PostFoundReport;
+import Myaong.Gangajikimi.postlost.entity.PostLost;
+import Myaong.Gangajikimi.postlostreport.entity.PostLostReport;
+import Myaong.Gangajikimi.sightcard.entity.SightCard;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,7 +40,7 @@ public class Member extends BaseEntity {
 	private String password;
 
 	@Column(nullable = false)
-	@ColumnDefault("'USER'")
+	@ColumnDefault("'ROLE_USER'")
 	@Enumerated(EnumType.STRING)
 	private Role role;
 
@@ -38,13 +49,35 @@ public class Member extends BaseEntity {
 	@Enumerated(EnumType.STRING)
 	private AccountStatus accountStatus;
 
+	@OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<PostLost> postLosts = new ArrayList<>();
+
+	@OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<PostFound> postFounds = new ArrayList<>();
+
+	@OneToMany(mappedBy = "reporter", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<SightCard> sightCards = new ArrayList<>();
+
+	@OneToMany(mappedBy = "reporter", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<PostLostReport> postLostReports = new ArrayList<>();
+
+	@OneToMany(mappedBy = "reporter", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<PostFoundReport> postFoundReports = new ArrayList<>();
+
+	@OneToMany(mappedBy = "member1", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<ChatRoom> ChatRooms = new ArrayList<>();
+
+	@OneToMany(mappedBy = "member2", cascade = CascadeType.REMOVE, orphanRemoval = true)
+	private List<ChatRoom> ChatRooms2 = new ArrayList<>();
+
+
 	@Builder
 	private Member(String memberName, String email, String password, String gender, Integer age, String address, String specAddress){
 
 		this.memberName = memberName;
 		this.email = email;
 		this.password = password;
-		this.role = Role.USER;
+		this.role = Role.ROLE_USER;
 		this.accountStatus = AccountStatus.ACTIVATED;
 	}
 
@@ -55,6 +88,10 @@ public class Member extends BaseEntity {
 			.email(request.getEmail())
 			.password(encryptedPassword)
 			.build();
+	}
+
+	public void changeStatus(String status) {
+		this.accountStatus = AccountStatus.valueOf(status.toUpperCase());
 	}
 
 
